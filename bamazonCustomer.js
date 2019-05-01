5ff4d3d80c8cfd333d386725e05e43016242636e
Chris Armata <chris.armata@gmail.com>
2019-05-01 01:33:50 -0400

    Add create order functionality

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,7 +3,8 @@ var inquirer = require("inquirer");
 var table = require("table").table;
 require("dotenv").config();
 
-var connection = mysql.createConnection({
+var pool = mysql.createPool({
+  connectionLimit: 10,
   host: "localhost",
   port: 3306,
   user: "root",
@@ -12,13 +13,10 @@ var connection = mysql.createConnection({
 });
 
 var app = () => {
-  showAllItems();
-};
-
-var showAllItems = () => {
-  connection.connect(err => {
+  pool.getConnection((err, conn) => {
     if (err) throw err;
-    connection.query("SELECT * FROM products", (err, res) => {
+
+    conn.query("SELECT * FROM products", (err, res) => {
       if (err) throw err;
       var results = [];
       results.push(["ID", "Product", "Price"]);
@@ -26,8 +24,45 @@ var showAllItems = () => {
         results.push([row.item_id, row.product_name, row.price]);
       });
       console.log(table(results));
-      connection.end();
     });
+
+    inquirer
+      .prompt([
+        {
+          type: "number",
+          name: "pickId",
+          message: "What is the ID of the product you would like to buy?"
+        },
+        {
+          type: "number",
+          name: "pickUnits",
+          message: "How many units of the product would you like to buy?"
+        }
+      ])
+      .then(res => {
+        var productId = res.pickId;
+        var productUnits = res.pickUnits;
+        conn.query("SELECT stock_quantity, price FROM products WHERE item_id = ?", productId, (err, res) => {
+          if (err) throw err;
+          var stock = res[0].stock_quantity;
+          var price = res[0].price;
+
+          if (stock > 0) {
+            conn.query(
+              "UPDATE products SET stock_quantity = ? WHERE item_id = ?",
+              [stock - productUnits, productId],
+              (err, res) => {
+                conn.destroy();
+                if (err) throw err;
+                var cost = price * productUnits;
+                return console.log(`The total cost of your order is: $${cost}`);
+              }
+            );
+          } else {
+            return console.log("Insufficient quantity!");
+          }
+        });
+      });
   });
 };
 

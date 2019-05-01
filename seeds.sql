a9a65d39dfb0c83d84ba38aedbbfee2a8ebdcbbb
Chris Armata <chris.armata@gmail.com>
2019-04-30 23:11:51 -0400

    Add seed data

---
+++ seeds.sql
@@ -0,0 +1,13 @@
+USE bamazon;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("T-Shirt", "Clothing", 10.99, 20),
+("Jeans", "Clothing", 15.99, 15),
+("Can of Beans", "Food", 6.99, 50),
+("Detergent", "Household", 10.99, 30),
+("TV", "Electronics", 599.99, 20),
+("iPhone", "Electronics", 899.99, 30),
+("Flash Drive", "Electronics", 12.99, 100),
+("Shovel", "Outdoors", 18.99, 40),
+("Garden Gloves", "Outdoors", 7.99, 50),
+("Chainsaw", "Outdoors", 169.99, 20);
